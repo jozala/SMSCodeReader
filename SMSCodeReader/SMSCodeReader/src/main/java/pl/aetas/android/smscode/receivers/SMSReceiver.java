16e16db2d46da44b68fb79b7b4d1890c95b07841
mariusz <mariusz@aetas.pl>
2013-05-29 01:01:48 +0100

    Simplified SMSReceiver prepared

---
+++ SMSCodeReader/SMSCodeReader/src/main/java/pl/aetas/android/smscode/receivers/SMSReceiver.java
@@ -0,0 +1,36 @@
+package pl.aetas.android.smscode.receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import pl.aetas.android.smscode.basic.SMSReader;
+
+public class SMSReceiver extends BroadcastReceiver {
+
+    public static final String SMS_EXTRA_NAME = "pdus";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Get the SMS map from Intent
+        Bundle extras = intent.getExtras();
+
+        if (extras != null) {
+            // Get received SMS array
+            Object[] smsExtra = (Object[]) extras.get(SMS_EXTRA_NAME);
+            for (final Object aSmsExtra : smsExtra) {
+                SmsMessage sms = SmsMessage.createFromPdu((byte[]) aSmsExtra);
+
+                String body = sms.getMessageBody();
+                String address = sms.getOriginatingAddress();
+
+                SMSReader smsReader = SMSReader.getInstance(context);
+                smsReader.readSMS(address, body);
+
+            }
+
+        }
+
+    }
+}
